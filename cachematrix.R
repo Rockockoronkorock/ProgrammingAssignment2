8262034b732e09f94b71cc63598d1a72bd02e90c
d054410 <felix.ruecker@sap.com>
2018-11-20 22:31:22 +0100

    Assignment Completion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I put the overall description into the Readme (see github repository)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Create a cache matrix (with random matrix if non specified)
+makeCacheMatrix <- function(x = replicate(3, rnorm(3))) { # Just a default random matrix for parameter
+  i <- NULL
+  set <- function(y) { 
+    if(!matequal(x, y)){ # Only set NULL if it really has been changed, otherwise keep cache
+      x <<- y
+      i <<- NULL 
+    }
+  }
+  get <- function() x
+  setInverse <- function(matrix) i <<- solve(matrix)
+  getInverse <- function() i
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+# Solves things
+cacheSolve <- function(mat, ...) {
+  inv <- mat$getInverse()
+  if(!is.null(inv)) {
+    message("Getting cached Inverse")
+    return(inv)
+  }
+  data <- mat$get()
+  inv <- solve(data, ...)
+  mat$setInverse(inv)
+  inv
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Helper Function, source: https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html
+matequal <- function(x, y) {
+  is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
 }
